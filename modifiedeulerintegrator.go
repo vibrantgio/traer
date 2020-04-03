66db0ee333d7f5f1a6303d9c9cf5ce58745d950f
René Post <github@xpt.nl>
2020-04-03 11:49:52 +0200

    Fix bug in calulation of position for modified euler, 0.5*t*t should be 0.5*(1/t)*(1/t).

--- modifiedeulerintegrator.go
+++ modifiedeulerintegrator.go
@@ -12,15 +12,15 @@ func (i *modifiedEulerIntegrator) Step(t float64) {
 	i.ps.ClearForces()
 	i.ps.ApplyForces()
 
-	halftt := 0.5 * t * t
-	oneovert := 1 / t
+	dt := 1 / t
+	halfdtdt := 0.5 * dt * dt
 
 	for _, p := range i.ps.Particles {
 		if !p.Fixed {
 			a := p.Force.Scale(1.0 / p.Mass)
-			p.Position.AddAssign(p.Velocity.Scale(oneovert))
-			p.Position.AddAssign(a.Scale(halftt))
-			p.Velocity.AddAssign(a.Scale(oneovert))
+			p.Position.AddAssign(p.Velocity.Scale(dt))
+			p.Position.AddAssign(a.Scale(halfdtdt))
+			p.Velocity.AddAssign(a.Scale(dt))
 		}
 	}
 }
