bedc07e96393a05dcc67e73ea5c93020cabd617c
René Post <github@xpt.nl>
2020-04-03 18:58:33 +0200

    Also add a standard verlet integrator. Stores Pprev in Velocity as P - Pprev, so Pprev can be recovered for the next step.

--- verletintegrator.go
+++ verletintegrator.go
@@ -1,5 +1,30 @@
 package traer
 
+func MakeDefaultVerletIntegrator(ps *ParticleSystem) Integrator {
+	return &defaultVerletIntegrator{ps: ps}
+}
+
+type defaultVerletIntegrator struct {
+	ps *ParticleSystem
+}
+
+func (i *defaultVerletIntegrator) Step(t float64) {
+	i.ps.ClearForces()
+	i.ps.ApplyForces()
+
+	dt := 1 / t
+	dtdt := dt * dt
+
+	for _, p := range i.ps.Particles {
+		if !p.Fixed {
+			a := p.Force.Scale(1.0 / p.Mass)
+			position := p.Position.Add(p.Velocity.Scale(dt)).Add(a.Scale(dtdt))
+			p.Velocity = position.Subtract(p.Position).Scale(t)
+			p.Position = position
+		}
+	}
+}
+
 func MakeVelocityVerletIntegrator(ps *ParticleSystem) Integrator {
 	return &velocityVerletIntegrator{ps: ps}
 }
